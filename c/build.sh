c2957b8fcef7bce35aae39faaa5ac2ef7f786d86
ChenlongWang <wangchenlong@anji-logistics.com>
2018-05-02 09:35:50 +0800

    add cuda

--- c/build.sh
+++ c/build.sh
@@ -1,3 +1,5 @@
 scons -c
 scons -Q
 python combine.py
+mv libchlwang.a ../lib
+mv chlwang.h ../include
---
+++ cuda/build.sh
@@ -0,0 +1,5 @@
+scons -c
+scons -Q
+python combine.py
+mv libchlwcuda.a ../lib
+mv chlwang_cuda.h ../include
